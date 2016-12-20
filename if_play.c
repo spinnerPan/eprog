36b02e4e37ebe036d3dd8140796759dfe83d6a25
Kyle Hurboda Loreto <loretokyle28@gmail.com>
2016-12-20 08:11:17 +0800

    Update if_play.c

--- if_play.c
+++ if_play.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 int main(){
 	int num;
+	
 	printf("Enter a number:");
 	scanf("%d", &num);
-		if (num%3==0){
-			printf("Ed ");
-		}
-		if (num%5==0){
-			printf("Pogi");
- 		}
+	if (num%3==0){
+		printf("Ed ");
+	}
+	if (num%5==0){
+		printf("Pogi");
+ 	}
 }
 	
