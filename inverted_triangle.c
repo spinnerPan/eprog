42ad633d1d6925e6a970936aa5426dfb22cd2151
Kyle Hurboda Loreto <loretokyle28@gmail.com>
2017-01-24 08:01:47 +0800

    Create inverted_triangle.c

---
+++ inverted_triangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){	
+	int a,b,c;
+	printf("Enter a number:");
+	scanf("%d",&a);
+	for(b=a; b>=0 ;b--){
+		for(c=0; c<b ;c++){
+			printf("*");
+		}
+		printf("\n");			
+	}
+	system("pause");
+}
